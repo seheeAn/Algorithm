cefb1a32aa611ccb3adf60d8c555f046b6c162c4
sehee <imash0525@gmail.com>
2024-01-16 02:48:32 +0900

    Improve - 1916 최소비용 구하기 (인접리스트 사용 및 최소 cost 갱신으로 메모리 문제 해결)

--- Boj/1916_최소비용구하기.py
+++ Boj/1916_최소비용구하기.py
@@ -1,14 +1,13 @@
 import heapq
-MAXNUM = 100000
 N = int(input()) #도시개수
 M = int(input()) #버스개수
 
 #메모리 초과로 인접리스트 사용 > 해도 메모리 초과;;;
-#해당 지점까지의 현재 cost를 저장하고 이것보다 cost가 작을 경우에만 que에 추가 >> 이것도 메모리초과 
+#해당 지점까지의 현재 cost를 저장하고 이것보다 cost가 작을 경우에만 cost갱신 및 que에 추가
 cities = [[] for _ in range(N+1)]
-costs = [MAXNUM for _ in range(N+1)]
+costs = [100000001 for _ in range(N+1)] #100000 * 1000 이 max값
 
-for i in range(M):
+for _ in range(M):
     a, b, c = map(int, input().split())
     cities[a].append([b,c]) #도착지, 비용
 
@@ -16,18 +15,24 @@ start, end = map(int, input().split())
 
 #초기값
 que = []
+res = 0
 heapq.heappush(que, (0, start))
 costs[start] = 0
 
-while(que):
+while que:
     cost, city = heapq.heappop(que)
+    
+    if city == end:
+        res = cost
+        break
+
     if costs[city] < cost:
         continue # 밑을 수행하지 않고 다음 iter로 넘어감
-    
+
     for c in cities[city]:
         next_cost = cost + c[1]
-        if next_cost <= costs[c[0]]: #방문한 적 없고, 지금까지 저장된 cost보다 작다면
+        if next_cost < costs[c[0]]: #해당 지점까지의 cost가 지금까지 저장된 cost보다 작다면
             heapq.heappush(que, (next_cost, c[0]))
             costs[c[0]] = next_cost #힙에 들어갈 때 갱신
 
-print(costs[end])
+print(res)
