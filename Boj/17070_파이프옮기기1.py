279327efc15bc7eda14fac3af1802b7d6354a276
sehee <imash0525@gmail.com>
2024-01-24 00:31:25 +0900

    Solve - dfs 사용 (모든 경우의 수를 구할 때는 bfs보다 dfs가 빠르다)

--- Boj/17070_파이프옮기기1.py
+++ Boj/17070_파이프옮기기1.py
@@ -1,31 +1,26 @@
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-move = [[0,1],[1,1],[1,0]] # 오른쪽, 대각선, 아래
-que = [[0,1,0]] #x,y,rotate(0,1,2) 가로,대각선,세로
-
 #가로>대각선 / 세로>대각선 / 대각선>가로 / 대각선>세로 / 대각선>대각선
 
 cnt = 0
-while que:
-    x, y, r = que.pop(0)
-    
+def dfs(x,y,r):
+    global cnt
     if x == N-1 and y == N-1:
         cnt += 1
+        return
     
-    for i in range(3):
-        
-        if r == 0 and i == 2: #가로일때 세로 이동이면
-            continue #해당 iter 실행 x
-        elif r == 2 and i == 0: #세로일때 가로 이동이면
-            continue #해당 iter 실행 x
+    if r == 0 or r == 1: #가로 이동
+        if y+1 < N and not arr[x][y+1]:
+            dfs(x, y+1, 0)
+    
+    if r == 2 or r == 1:
+        if x+1 < N and not arr[x+1][y]: #세로이동
+            dfs(x+1, y, 2)
+    
+    if r==0 or r==1 or r==2:
+        if x+1 < N and y+1 < N:
+         if not arr[x+1][y] and not arr[x][y+1] and not arr[x+1][y+1]: #대각선이동
+            dfs(x+1, y+1, 1)
 
-        new_x = x + move[i][0]
-        new_y = y + move[i][1]
-        new_r = i
-
-        if new_x < N and new_y < N: #범위 확인
-            if not (i == 1 and (arr[new_x][y] == 1 or arr[x][new_y] == 1)): #대각선일 경우 위, 옆 체크
-                if arr[new_x][new_y] == 0: #벽이 아니면
-                    que.append([new_x,new_y,new_r])
-
+dfs(0,1,0)
 print(cnt)
