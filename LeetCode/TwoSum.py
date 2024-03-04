7d55456a42d09cb8eb6de9008f2c59c093b2f1e3
sehee <imash0525@gmail.com>
2024-03-04 21:24:56 +0900

    Solve - TwoSum: for문 구현
    #6

---
+++ LeetCode/TwoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        answer = []
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    answer.append(i)
+                    answer.append(j)
+        
+        return answer
+
+        
