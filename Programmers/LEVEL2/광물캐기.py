8db1e3670631a0f0a9ed91f193bb3ee5b0f759f2
sehee <imash0525@gmail.com>
2024-03-22 21:13:44 +0900

    Solve - 완전탐색, dfs

---
+++ Programmers/LEVEL2/광물캐기.py
@@ -0,0 +1,40 @@
+def solution(picks, minerals):
+    answer = 25 * len(minerals)
+    # picks = [dia, iron, stone]
+    tire_list = [[1,1,1],[5,1,1],[25,5,1]]
+    for i in range(len(minerals)):
+        if minerals[i] == "diamond":
+            minerals[i] = 0
+        elif minerals[i] == "iron":
+            minerals[i] = 1
+        else:
+            minerals[i] = 2
+
+    # dfs
+    def dfs(idx, tired, li):
+        nonlocal answer
+        if idx == len(minerals) or li == [0, 0, 0]: # 종료 조건
+            if answer > tired:
+                answer = tired
+                return
+        
+        for i in range(len(li)):
+            if li[i]: # 0이 아니면
+                li[i] -= 1
+                # 캐기
+                cnt = 0
+                n_idx = idx
+                n_tired = tired
+                while n_idx < len(minerals):
+                    cnt += 1
+                    n_tired += tire_list[i][minerals[n_idx]]
+                    n_idx += 1
+                    if cnt == 5:
+                        break
+                dfs(n_idx, n_tired, li)
+                li[i] += 1
+        return
+    
+    dfs(0, 0, picks)
+    
+    return answer
