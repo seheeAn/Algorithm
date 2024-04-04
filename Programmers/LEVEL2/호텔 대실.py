0b090841c85f10e4ca71eed7f40c1babc518dc4b
sehee <imash0525@gmail.com>
2024-04-04 16:44:34 +0900

    Improve - 누적합

--- Programmers/LEVEL2/호텔 대실.py
+++ Programmers/LEVEL2/호텔 대실.py
@@ -25,3 +25,23 @@ def solution(book_time):
             room_list.append(end+10)
         
     return answer
+
+### 누적합으로 풀기 ###
+def solution(book_time):
+    time_list = [0 for _ in range(24*60 + 10)]
+    #시간 변환
+    book_list = []
+    for book in book_time:
+        s_hh, s_mm = map(int, book[0].split(":"))
+        e_hh, e_mm = map(int, book[1].split(":"))
+        start = s_hh*60 + s_mm
+        end = e_hh*60 + e_mm + 10
+        time_list[start] += 1
+        time_list[end] -= 1
+    
+    num = 0
+    for i, time in enumerate(time_list):
+        num += time
+        time_list[i] = num
+        
+    return max(time_list)
